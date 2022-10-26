9f54df3089a1d1c5d53d455f6864aa8ddd5fb376
Chris Rodgers <cj.rodgers@gmail.com>
2022-10-26 17:25:13 +0100

    commented out url param code

--- src/about.js
+++ src/about.js
@@ -2,21 +2,20 @@ import { Grid, Typography } from "@mui/material";
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
-class About extends Component {
-  render() {
-    return (
-      <Grid container sx={{ px: 3 }}>
-        <Grid md="6" textAlign="center" sx={{ mx: "auto", mt: 5 }}>
-          <Typography paragraph={true}>This is the about page</Typography>
-        </Grid>
-        <Grid item lg={7} md={8} xs={12} textAlign="center" pb={2}>
-          <NavLink to={"/"}>
-            Home
-          </NavLink>
-        </Grid>
+const About = () => {
+  // const urlParams = new URLSearchParams(window.location.search);
+  // let author = urlParams.get("author");
+
+  return (
+    <Grid container sx={{ px: 3 }}>
+      <Grid md="6" textAlign="center" sx={{ mx: "auto", mt: 5 }}>
+        <Typography paragraph={true}>This is the about page</Typography>
       </Grid>
-    );
-  }
-}
+      <Grid item lg={7} md={8} xs={12} textAlign="center" pb={2}>
+        <NavLink to={"/"}>Home</NavLink>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default About;
